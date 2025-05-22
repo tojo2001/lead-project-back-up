061597f3f903eec4aed6b06511eb96184554c3f3
Tojonirina RANDRIAMAMPIONONA <t.randriamampionona.ext@oppycx.com>
2025-05-22 18:07:09 +0200

    UI: add close button

--- components/table/section-table.tsx
+++ components/table/section-table.tsx
@@ -6,9 +6,12 @@ import { LeadTable } from "./lead-table";
 import ActionBtnSection from "../action-btn-section";
 import { useLeadStore } from "@/store/use-lead.store";
 import { toast } from "../ui/use-toast";
+import { X } from "lucide-react";
+import { useRef } from "react";
 
 export function TableSection() {
   const { filteredLeadData, resetFilterLead } = useLeadStore();
+  const btnRef = useRef<HTMLButtonElement | null>(null);
 
   const onClose = () => {
     if (!filteredLeadData) return;
@@ -25,9 +28,18 @@ export function TableSection() {
   return (
     <Drawer onClose={onClose}>
       <DrawerTrigger asChild>
-        <Button variant="outline">Veiw as Table</Button>
+        <Button variant="outline" ref={btnRef}>
+          Veiw as Table
+        </Button>
       </DrawerTrigger>
       <DrawerContent>
+        <Button
+          size="icon"
+          className="absolute -top-28 left-1/2 -translate-x-1/2 rounded-full"
+          onClick={() => btnRef.current?.click()}
+        >
+          <X size={18} />
+        </Button>
         <div className="mx-auto w-full max-w-[calc(100%-2rem)]">
           <LeadTable />
         </div>
